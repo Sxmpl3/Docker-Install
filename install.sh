e0b8bf82a0cbe143d9902fb5f4ca094716df32eb
Sxmpl3 <116309678+Sxmpl3@users.noreply.github.com>
2024-04-16 09:34:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,81 @@
 #!/bin/bash
 
-yum install -y docker
-systemctl start docker
-systemctl enable docker
-yum install -y docker-compose
+# SCRIPT CREADO PARA LA INSTALACION DE FORMA AUTOMATICA 4 DOCKERS DISTINTOS EN UNA INSTANCIA EC2 DE AMAZON, CADA CONTENEDOR SE REENVIA A OTRO PUERTO DISTINTO
 
-cat <<EOF > docker-compose.yml
-version: '3.7'
-services:
-  wordpress:
-    image: wordpress
-    ports:
-      - "80:80"
-    environment:
-      WORDPRESS_DB_HOST: db
-      WORDPRESS_DB_USER: wordpress
-      WORDPRESS_DB_PASSWORD: my_password
-      WORDPRESS_DB_NAME: wordpress
-    volumes:
-      - wordpress:/var/www/html
-  db:
-    image: mysql:5.7
-    environment:
-      MYSQL_DATABASE: wordpress
-      MYSQL_USER: wordpress
-      MYSQL_PASSWORD: my_password
-      MYSQL_ROOT_PASSWORD: my_root_password
-    volumes:
-      - db:/var/lib/mysql
-volumes:
-  wordpress:
-  db:
-EOF
+if [[ $1 == "" ]];then
+    echo -e "[USE EL SIGUIENTE PARAMETRO PARA APRENDER USAR LA HERRAMIENTA]"
+    echo -e "--------------------------------------------------------------"
+    echo -e "root@0.0.0.0: /home/admin# ./dockerinstall.sh -h"
+    echo ""
+fi
 
-sudo docker-compose up -d
+if [[ $1 == "-h" ]];then
+    echo -e "Script creado por https://github.com/Sxmpl3"
+    echo -e "-------------------------------------------"
+    echo -e "./dockerinstall.sh -d [INSTALACION DE DOCKER]"
+    echo -e "./dockerinstall.sh -w [INSTALACION DE CONTENEDOR WORDPRESS]"
+    echo -e "./dockerinstall.sh -m [INSTALACION DE CONTENEDOR MARIADB]"
+    echo -e "./dockerinstall.sh -l [INSTALACION DE CONTENEDOR LOGIN]"
+    echo -e "./dockerinstall.sh -r [INSTALACION DE CONTENEDOR REGISTER]"
+    echo -e "./dockerinstall.sh -k [SERVICIO DE MONITORIZACIÓN]"
+    echo -e ""
+fi
+
+if [[ $1 == "-d" ]];then
+    echo -e "\n[-] Instalando docker y las imagenes necesarias...(esto puede llevar algo de tiempo)"
+    apt install docker docker-compose -y > /dev/null 2>&1
+    echo -e ""
+    docker pull wordpress > /dev/null 2>&1
+    docker pull mysql > /dev/null 2>&1
+    docker pull sxmpl3/login-app > /dev/null 2>&1
+    docker pull sxmpl3/register-app > /dev/null 2>&1
+    echo -e "\n[+] Instalado con éxito"
+    echo -e ""
+fi
+
+if [[ $1 == "-w" ]];then
+    echo -e "\n[-] Iniciando contenedor de Wordpress en el puerto 80..."
+    docker container run -d --name wordpress -p 80:80 wordpress > /dev/null 2>&1
+    echo -e ""
+    echo -e "\n[+] Iniciado con éxito"
+    echo -e ""
+fi
+
+if [[ $1 == "-m" ]];then
+    echo -e "\n[-] Iniciando contenedor de MySQL en el puerto 3333..."
+    docker run --name mysql -e MYSQL_ROOT_PASSWORD=@Puente22 -d -p 3333:3306 \
+    -e MYSQL_DATABASE=credentials \
+    mysql:latest > /dev/null 2>&1
+    echo -e ""
+    echo -e "\n[+] Accediendo al contenedor de MYSQL..."
+    echo -e ""
+    docker exec -it mysql /bin/bash
+fi
+
+if [[ $1 == "-l" ]];then
+   echo -e "\n[-] Iniciando contenedor de Login en el puerto 8080..."
+   echo -e ""
+   docker run --name login -d -p 8080:80 sxmpl3/login-app > /dev/null 2>&1
+   echo -e "\n[+] Accediendo al contenedor de Login..."
+   echo -e ""
+   docker exec -it login /bin/bash
+fi 
+
+if [[ $1 == "-r" ]];then
+   echo -e "\n[-] Iniciando contenedor de Register en el puerto 8081..."
+   echo -e ""
+   docker run --name register -d -p 8081:80 sxmpl3/register-app  > /dev/null 2>&1
+   echo -e "\n[+] Accediendo al contenedor de Register..."
+   echo -e ""
+   docker exec -it register /bin/bash
+fi
+
+if [[ $1 == "-k" ]];then
+    echo -e "\n[-] Iniciando servicio de monitorización..."
+    echo -e ""
+    docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name kuma louislam/uptime-kuma:1 > /dev/null 2>&1
+    echo -e "\n[+] Iniciado con éxito"
+    echo -e ""
+fi
+
+
